f56606ac99b4b4c1e71b6b287946d9d44193e6f9
Vamp <25523682+the-vampiire@users.noreply.github.com>
2019-05-05 17:55:31 -0400

    code: starter code

---
+++ code/src/Routes.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+const Routes = () => (
+  <Switch>
+    {/* we will implement these Route components later */}
+    <Route exact path="" component={} />
+  </Switch>
+);
+
+export default Routes;
